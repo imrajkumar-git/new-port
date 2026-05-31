f4d07fd526f13192e8d060377b63f8f79e070ecb
Rajkumar <rajkumararyal0977@gmail.com>
2026-05-31 10:02:57 +0400

    add

---
+++ src/data/BlogData.js
@@ -0,0 +1,71 @@
+// blog data
+
+export const Blogs = [
+    {
+        id:1,
+        name:"Build Website with ReactJS, Styled-components and GSAP for Scrolling Animations",
+        tags:["react","gsap","styled-components"],
+        date:"13 May, 2021",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:"https://dev.to/codebucks/build-website-with-reactjs-styled-components-and-gsap-for-scrolling-animations-2f10"
+    },
+    {
+        id:2,
+        name:"How to create React Hamburger Menu using Styled-Components",
+        tags:["react","styled-components"],
+        date:"5 July, 2021",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:"https://codebucks.hashnode.dev/react-hamburger-menu"
+    },
+    {
+        id:3,
+        name:"React Loading Screen: Try these 3 cool loading screens for your app",
+        tags:["react","react-lottie","styled-components"],
+        date:"6 July, 2021",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--sBeV06Xc--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/2yrzy75q4dzibqz6rf82.png",
+        link:"https://codebucks.hashnode.dev/react-loading-screens"
+    },
+    {
+        id:4,
+        name:"How to build a Fabulous Todo App using React, Redux and Framer-Motion",
+        tags:["react","redux","framer-motion"],
+        date:"26 May, 2021",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+        link:"https://dev.to/codebucks/build-redux-react-todo-list-app-with-animations-using-framer-motion-1mp1"
+    },
+    {
+        id:5,
+        name:"What is Higher Order Component (HOC) in React JS?",
+        tags:["react","reactJS","web-development"],
+        date:"3 Feb, 2021",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.png",
+        link:"https://codebucks.hashnode.dev/what-is-higher-order-component"
+    },
+    {
+        id:6,
+        name:"How to implement Pagination Component in React from scratch",
+        tags:["react","css"],
+        date:"5 Jan, 2021",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.png",
+        link:"https://codebucks.hashnode.dev/pagination-in-react"
+
+    },
+   
+    {
+        id:7,
+        name:"What is Redux ? Simply Explained!",
+        tags:["react","redux","javascript"],
+        date:"16 May, 2021",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--ZY1EK-Eo--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/bcz1u90oq3prr5fga3fs.png",
+        link:"https://dev.to/codebucks/what-is-redux-simply-explained-2ch7"
+    },
+    {
+        id:8,
+        name:"How to create Sidebar navigation menu in ReactJS with react router and framer-motion",
+        tags:["react","framer-motion","styled-components"],
+        date:"26 May, 2021",
+        imgSrc:"https://res.cloudinary.com/practicaldev/image/fetch/s--rv4I8UwE--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0a9h50milhlf1us6mn4a.png",
+        link:"https://dev.to/codebucks/build-sidebar-navigation-menu-in-reactjs-with-react-router-and-framer-motion-for-cool-page-transition-effects-4bc3"
+    },
+  
+] 
